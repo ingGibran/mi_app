ba395119c2bacba9a06ef4984cbb13b26f954f01
ingGibran <zaga1709@hotmail.com>
2026-04-28 09:45:40 -0600

    workflow

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
 def main():
     print("Mi BOMBOOOOOO")
+    return "Mi Bomboooooo"
 
 
 if __name__ == "__main__":
     main()
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        log_level="info"
+    )
